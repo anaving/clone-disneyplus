f9c4c1bd41430e99b0c99024af42e091ec0f36e6
anaving <contato.unf@gmail.com>
2023-04-12 20:56:55 -0300

    Corrigindo o css button, inclusão do footer.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]'); // adicionado o data-faq-questions em todas as questoes para que fiquem relacionadas, não precisa colocar atributo.
     
     
     for (let i = 0; i < buttons.length; i++) {
@@ -12,7 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta); /* chama uma função */
+    }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open'; //  se esta presente no elemento está aberto, caso contrario está fechado
+    console.log(elemento);
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe); // elemento pai com uma class list com abrir e fechar chamando a const classe de aberto logo acima
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -22,7 +35,7 @@ function removeBotaoAtivo() {
     }
 }
 
-function escondeTodasAbas() {
+function escondeTodasAbas(elemento) {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for(let i = 0; i < tabsContainer.length; i++) {
