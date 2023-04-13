ffb0ba64e0bbb1b993dfae3878f328386d97656b
anaving <contato.unf@gmail.com>
2023-04-12 22:09:13 -0300

    Finalizando

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]'); // adicionado o data-faq-questions em todas as questoes para que fiquem relacionadas, não precisa colocar atributo.
     
-    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY; // scrollY para o eixo vertical
+        // verificação: quando o valor de window.scrollY for maior que const alturaHero pode exibir os demais elementos
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader(); // chamando a função abaixo de ocultar
+        } else {
+            exibeElementosDoHeader();
+        }
+
+    })
+
+    // seção de atrações, programação das abas
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -14,10 +30,21 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // seção FAQ accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta); /* chama uma função */
     }
 })
+
+function ocultaElementosDoHeader() { // oculta
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden'); // vai chamar a função pra ver a posição do scroll logo acima
+}
+
+function exibeElementosDoHeader() { //exibe
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden'); // vai chamar a função pra ver a posição do scroll logo acima
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open'; //  se esta presente no elemento está aberto, caso contrario está fechado
